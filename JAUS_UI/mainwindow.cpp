37e294217de093b64104845d90d1f428a0fba2e3
David Hodo <david.hodo@gmail.com>
2012-04-03 09:30:21 -0500

    save before changing column layout on discovery tree view

--- JAUS_UI/mainwindow.cpp
+++ JAUS_UI/mainwindow.cpp
@@ -64,11 +64,14 @@ void MainWindow::on_pbQueryServices_clicked()
     std::map<short, openjaus::model::Subsystem * > subsystems;
     std::map<unsigned char, openjaus::model::Node *> nodes;
     std::map<unsigned char, openjaus::model::Component * > components;
+    std::map< std::string, openjaus::model::Service * > services;
+
     subsystems = JAUSComponent->getSystemTree()->getSubsystems();
 
     typedef std::map<short, openjaus::model::Subsystem* >::iterator it_subsys;
     typedef std::map<unsigned char, openjaus::model::Node* >::iterator it_node;
     typedef std::map<unsigned char, openjaus::model::Component * >::iterator it_comp;
+    typedef std::map<std::string, openjaus::model::Service * >::iterator it_serv;
 
     QStandardItemModel *standardModel = new QStandardItemModel ;
 
@@ -92,8 +95,25 @@ void MainWindow::on_pbQueryServices_clicked()
         prevItem->appendRow(subsysRow);
         for (it_node it_n = nodes.begin(); it_n!=nodes.end(); it_n++) {
             components=it_n->second->getComponents();
+            QList<QStandardItem *> nodeRow;
+            nodeRow << new QStandardItem("Node:");
+            nodeRow << new QStandardItem(QString::number(it_n->second->getId()));
+            nodeRow << new QStandardItem(QString::fromUtf8(it_n->second->getName().c_str()));
+            subsysRow[0]->appendRow(nodeRow);
             for (it_comp it_c = components.begin(); it_c!=components.end(); it_c++) {
-
+                QList<QStandardItem *> compRow;
+                compRow << new QStandardItem("Comp:");
+                compRow << new QStandardItem(QString::number(it_c->second->getId()));
+                compRow << new QStandardItem(QString::fromUtf8(it_c->second->getName().c_str()));
+                nodeRow[0]->appendRow(compRow);
+                services = it_c->second->getServices();
+                for (it_serv it_s = services.begin(); it_s!=services.end(); it_s++) {
+                    QList<QStandardItem *> servRow;
+                    servRow << new QStandardItem("");
+                    servRow << new QStandardItem("");
+                    servRow << new QStandardItem(QString::fromUtf8(it_s->second->toString().c_str()));
+                    compRow[0]->appendRow(servRow);
+                }
             }
         }
     }
